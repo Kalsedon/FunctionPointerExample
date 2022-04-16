906c7af25c869fd4a7b48fd755c2fa748ccda79e
Serhat Kılbaş <serhatkilbas00@hotmail.com>
2022-04-16 14:20:54 +0300

    passing a lambda which is on hand to the function

--- FunctionPointer/main.cpp
+++ FunctionPointer/main.cpp
@@ -30,8 +30,14 @@ int main()
 	strings.push_back("a");
 	strings.push_back("t");
 	
+	auto print = [](std::string& s) -> void // return type can be specified like that
+	{
+		std::cout << s << std::endl;
+	};
+
 	auto newfunction = Print;
 	Print(strings, PrintAnyThing); // alternatively Print(strings, newfunction), it does the same thing or -->
 	Print(strings, [](std::string& s) {std::cout << s << std::endl; }); // passing a lambda (throwaway function)
+	Print(strings, print);
 	std::cin.get();
 }
